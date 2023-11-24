9894fb6d99ed7e1e2203ab17d25ccf234c7ec261
llcain <llcain2012@gmail.com>
2023-11-24 15:16:16 -0600

    Added padding to span

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <h1>React - Sass - UseState</h1>
       <button type="button" className='btn btn-primary' onClick={() => setCount(count + 1)}>+</button>
-      <span>{count}</span>
+      <span id='badge'>{count}</span>
       <button type="button" className='btn btn-success' onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
